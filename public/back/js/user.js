9d00f971e0d5b741c941761af29e9c5a0552e6b7
ajingbo <amajingbo@163.com>
2019-06-30 16:02:27 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -24,9 +24,65 @@ $(function(){
                 console.log(info);
                 var htmlStr = template("tpl",info);
                 $('.lt_content tbody').html(htmlStr);
+
+                //分页初始化
+                $('#paginator').bootstrapPaginator({
+                    //指定bootstrap的版本
+                    bootstrapMajorVersion: 3,
+                    //总页数
+                    totalPages:Math.ceil(info.total/info.size),
+                    //当前页
+                    currentPage:info.page,
+                    //给所有的按钮添加页码点击事件
+                    onPageClicked:function(a,b,c,page){
+                        //更新当前页
+                        currentPage = page;
+                        //重新渲染
+                        render();
+                    }
+
+                })
             }
 
         })
     }
     render();
+
+    //2-给所有禁用启用按钮 添加点击事件（通过事件委托绑定）
+    $('tbody').on('click',".btn",function() {
+        //console.log("按钮被点击了");
+        //显示模态框
+        $('#userModal').modal("show");
+        //通过自定义属性 获取td中存在的用户ID 并保存在全局变量中
+        currentId = $(this).parent().data("id");
+        //1 启用 0 禁用
+        //通过判断类名 决定需要传递给后台isDelete
+        // 如果是禁用按钮, 想要禁用该用户, 就是将该用户状态, 变成 0, 传 0
+        isDelete = $(this).hasClass("btn-danger")? 0 : 1;
+     //3-点击模态框的确定按钮 实现启用禁用 切换
+        $('#submitBtn').click(function(){
+            console.log("切换启用禁用状态");
+            //发送ajax请求
+            $.ajax({
+                type:"post",
+                url:"/user/updateUser",
+                data:{
+                    id:currentId,
+                    isDelete:isDelete
+                },
+                dataType:"json",
+                success:function(info) {
+                    console.log(info);
+                    if(info.success){
+                        //修改状态成功
+                        //关闭模态框
+                        $('#userModal').modal('hide');
+                        //页面重新渲染
+                        render();
+                    }
+                }
+            })
+        })
+
+    })
 })
